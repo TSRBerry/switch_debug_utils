03bcd6e2a2d688cf56022fd12a778806cf8cce94
TSR Berry <20988865+TSRBerry@users.noreply.github.com>
2022-09-11 23:39:21 +0200

    Improve formatting of print_debug.py

--- gdb_scripts/python3/pwndbg/print_debug.py
+++ gdb_scripts/python3/pwndbg/print_debug.py
@@ -6,8 +6,10 @@ import os
 PRINT_LOG = True
 REMOVE_LOGFILE = False
 WRITE_LOGFILE = True
+
 NOW = datetime.now().strftime("%Y%m%d%H%M%S")
 LOGNAME = f"print_debug_breakpoint-{NOW}.log"
+
 
 def delete_PDB_handler(event):
     if isinstance(event, PrintDebugBreakpoint):
@@ -17,11 +19,13 @@ def delete_PDB_handler(event):
             os.remove(LOGNAME)
             print("Done.")
 
+
 def exit_handler(event):
     if REMOVE_LOGFILE and WRITE_LOGFILE and os.path.isfile(LOGNAME):
         print("Removing file...")
         os.remove(LOGNAME)
         print("Done.")
+
 
 class PrintDebugBreakpoint(gdb.Breakpoint):
     # r0 = gdb.selected_inferior().architecture().registers().find("r0")
@@ -33,7 +37,8 @@ class PrintDebugBreakpoint(gdb.Breakpoint):
 
         # maybe events.inferior_call could be used instead (would not block anything)
         log_string = gdb.execute("x/s $r0", to_string=True)
-        processed_string = codecs.decode(log_string, "unicode-escape").lstrip('\t 0123456789xabcdef:').rstrip('" \n').lstrip('"')
+        processed_string = codecs.decode(
+            log_string, "unicode-escape").lstrip('\t 0123456789xabcdef:').rstrip('" \n').lstrip('"')
         if WRITE_LOGFILE:
             with open(LOGNAME, "a") as file:
                 file.write(processed_string + "\n")
@@ -50,5 +55,6 @@ class PrintDebugBreakpoint(gdb.Breakpoint):
 PrintDebugBreakpoint("svcOutputDebugString")
 gdb.events.breakpoint_deleted.connect(delete_PDB_handler)
 gdb.events.exited.connect(exit_handler)
+
 if WRITE_LOGFILE:
     print(f"Writing log to file: {LOGNAME}")
